d9de562296627de90653db41d85c748af82c3026
GanonKuppa <qtfdl94q@gmail.com>
2019-11-09 22:13:47 +0900

    adis16470から取得した角速度に下限カットを追加

--- src/robot_object/adis16470.h
+++ src/robot_object/adis16470.h
@@ -6,6 +6,7 @@
 #include "parameterManager.h"
 #include "timer.h"
 #include "myUtil.h"
+#include "math.h"
 #include <deque>
 #include <functional>
 
@@ -84,6 +85,7 @@ public:
         
         float omega_pre = omega_f[2];        
         omega_f[2] = GYRO32BIT_SCALE * scaler * (double)omega_c[2];
+        if(fabs(omega_f[2]) < 0.01)omega_f[2] = 0.0;
         alpha = (omega_f[2] - omega_pre) / DELTA_T;
 
         // ACC Y
